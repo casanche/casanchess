0d48cc847937dbfaf0672d9098fe27356b0a56c4
casanche <carlos.sanchez.mayordomo@gmail.com>
2025-07-19 01:03:39 +0200

    fix: tests with u64 values

--- tests/test-Utils.cpp
+++ tests/test-Utils.cpp
@@ -36,38 +36,38 @@ TEST(Utils, RandomNumberGenerator_64bits) {
 
 //BitboardUtils
 TEST(BitboardUtils, GetBit) {
-    EXPECT_FALSE( GetBit(0, 0) );
-    EXPECT_FALSE( GetBit(0, 50) );
+    EXPECT_FALSE( GetBit(ZERO, 0) );
+    EXPECT_FALSE( GetBit(ZERO, 50) );
     EXPECT_FALSE( GetBit(0x9102289248220842, 16) );
     EXPECT_TRUE( GetBit(0x9102289248220842, 63) );
 }
 TEST(BitboardUtils, BitscanForward) {
-    EXPECT_EQ(BitscanForward(0), -1); //expected behaviour for empty bitboards
-    EXPECT_EQ(BitscanForward(0x4000000000010), 4); //Bit index: 4 and 50
-    EXPECT_EQ(BitscanForward(0x4000000000000), 50); //Bit index: 50
+    EXPECT_EQ(BitscanForward(ZERO), -1); //expected behaviour for empty bitboards
+    EXPECT_EQ(BitscanForward((u64)0x4000000000010), 4); //Bit index: 4 and 50
+    EXPECT_EQ(BitscanForward((u64)0x4000000000000), 50); //Bit index: 50
 }
 TEST(BitboardUtils, BitscanReverse) {
-    EXPECT_EQ(BitscanReverse(0), -1);
-    EXPECT_EQ(BitscanReverse(0x4000000000010), 50); //Bit index: 4 and 50
-    EXPECT_EQ(BitscanReverse(0x10), 4); //Bit index: 4
+    EXPECT_EQ(BitscanReverse(ZERO), -1);
+    EXPECT_EQ(BitscanReverse((u64)0x4000000000010), 50); //Bit index: 4 and 50
+    EXPECT_EQ(BitscanReverse((u64)0x10), 4); //Bit index: 4
 }
 TEST(BitboardUtils, PopCount) {
-    EXPECT_EQ(PopCount(0),0);
-    EXPECT_EQ(PopCount(0x4000000000010),2);
-    EXPECT_EQ(PopCount(0x9102289248220842),16);
+    EXPECT_EQ(PopCount(ZERO),0);
+    EXPECT_EQ(PopCount((u64)0x4000000000010),2);
+    EXPECT_EQ(PopCount((u64)0x9102289248220842),16);
 }
 TEST(BitboardUtils, ResetLsb) {
     Bitboard b = 0x3102289248220840;
     EXPECT_EQ(ResetLsb(b), 6); //index returned
     EXPECT_EQ(b, (u64)0x3102289248220800); //LSB reset
     b = ZERO;
-    EXPECT_EQ(b, (u64)0);
+    EXPECT_EQ(b, ZERO);
     b = ONE;
-    EXPECT_EQ(b, (u64)1);
+    EXPECT_EQ(b, ONE);
 }
 TEST(BitboardUtils, IsolateLsb) {
     Bitboard b = 127;
-    EXPECT_EQ(IsolateLsb(b), (u64)1);
+    EXPECT_EQ(IsolateLsb(b), ONE);
     b = 3377802801971200;
     EXPECT_EQ(IsolateLsb(b), (u64)131072);
 }
