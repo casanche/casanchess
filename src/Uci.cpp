dbe463017734285fb93c57319ff04b96494e0fb8
casanche <carlos.sanchez.mayordomo@gmail.com>
2025-07-25 21:13:33 +0200

    feat: bench: change positions

--- src/Uci.cpp
+++ src/Uci.cpp
@@ -148,14 +148,15 @@ void Uci::Bench(int depth) {
     std::vector<std::string> testPositions = {
         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", // Starting position
         "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", // Kiwipete
-        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", // Position 3
-        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", // Position 4
-        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", // Position 5
-        "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10", // Position 6
-        "rnbqkb1r/pppp1ppp/5n2/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1", // Sicilian
-        "rnbqkb1r/pppp1ppp/5n2/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 3 2", // Sicilian after Nf3
-        "rnbqkb1r/pppp1ppp/5n2/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 4 3", // Sicilian after Nf3 Nf6
-        "rnbqkb1r/pppp1ppp/5n2/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 5 3"  // Sicilian after Nf3 Nf6 e3
+        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8", // Perft 5
+        "8/5p2/2B2k2/p7/1pr5/4r3/P1P3P1/3R2K1 w - - 12 48", // RG4 - Ending
+        "r1bqr1k1/ppp2ppp/3p1n2/4n3/2PNP3/2P2P2/P3B1PP/1RBQ1RK1 b - - 1 11", // RG16 - Opening
+        "6r1/1ppnr1kp/pq1b1p2/2p1pPP1/P3P3/1P1P1N2/R1PB3Q/5R1K b - - 2 30", // RG10 - Midgame Closed
+        "r3qb1k/1b4p1/p2pr2p/3n4/Pnp1N1N1/6RP/1B3PP1/1B1QR1K1 w - - 0 1", // Nolot 1 - Wide tree
+        "rnbq1b1r/p1pp1p1p/4k3/1p1NP1p1/2QP1p2/5N2/PP1B1KPP/n6R/ w - -", // Albillo 11 - Checks and mates
+        "8/3P4/n2K2kp/2p3nN/1b6/2p1p1P1/8/3B4 w - - 4 3"  // Quiescence and deep pruning
+        "8/4RNpk/p7/r1pr4/5P2/2P3K1/b2R4/8 w - - 1 40", // Lichess study - Search extensions 1
+        "8/p7/2k1p3/5p1p/2KP1P1P/P7/8/8 b - - 0 37", // Lichess study - Pawn Endgame 1
     };
 
     u64 totalNodes = 0;
@@ -169,6 +170,7 @@ void Uci::Bench(int depth) {
 
     for(size_t i = 0; i < testPositions.size(); i++) {
         std::cout << "Position " << (i + 1) << "/" << testPositions.size() << ":" << std::endl;
+        std::cout << testPositions[i] << std::endl;
 
         // Set position
         m_board.SetFen(testPositions[i]);
