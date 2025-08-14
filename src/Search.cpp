5dfd70b91ee65a98e5e08154c2d3ea9c474e3b29
casanche <carlos.sanchez.mayordomo@gmail.com>
2025-08-15 01:58:17 +0200

    lmr: new formula base
    bench 1378081

--- src/Search.cpp
+++ src/Search.cpp
@@ -843,17 +843,9 @@ int Search::LateMoveReductions(int moveScore, int depth, int moveNumber, bool is
     const int MULT_FACTOR = 100; 
 
     // History moves
-    if(moveScore < 180) {
-        lmr_value = -50 - 200*(isPV)
-            + (
-                - (20 * logScore)
-                + (200 * logDepth)
-                + (30 * logMoveNumber)
-            ) / LOG_TABLE_SCALE
-            + (
-                - (30 * logScore * logDepth)
-                + (15 * logScore * logMoveNumber)
-            ) / (LOG_TABLE_SCALE * LOG_TABLE_SCALE);
+    if(moveScore < 180 && !isPV) {
+        lmr_value = 50 + (75 * logDepth * logMoveNumber) / (LOG_TABLE_SCALE * LOG_TABLE_SCALE);
+        lmr_value += -50 * logScore / LOG_TABLE_SCALE;
     }
 
     // SEE << 0: very bad captures
