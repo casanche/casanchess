6eb2bbf7630c4bef023645fbf7fd9e554a9204ad
Carlos Sanchez Mayordomo <karshan7@gmail.com>
2025-08-31 12:07:26 +0200

    capture history: d*d (from 16*d)
    bench 1613849

--- include/Heuristics.h
+++ include/Heuristics.h
@@ -126,7 +126,7 @@ public:
     }
 private:
     int Bonus(int depth) const {
-        int bonus = 16 * depth;
+        int bonus = depth * depth;
         return std::min(MAX_BONUS, bonus);
     }
     int PieceIndex(const Move& move) const {
