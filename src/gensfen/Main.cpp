54800aa18292a156e7dc1dd8fe239dbf3b553502
Carlos Sanchez Mayordomo <karshan7@gmail.com>
2026-02-16 15:58:26 +0100

    feat(gensfen): initialize nnue and syzygy runtime

--- src/gensfen/Main.cpp
+++ src/gensfen/Main.cpp
@@ -1,8 +1,10 @@
-#include "Uci.h"
 #include "gensfen/GenSFen.h"
 
 #include "Attacks.h"
 #include "Evaluation.h"
+#include "NNUE.h"
+#include "Syzygy.h"
+#include "Uci.h"
 #include "ZobristKeys.h"
 
 #include <iostream>
@@ -12,13 +14,17 @@
 int main(int argc, char** argv) {
     Attacks::Init();
     Evaluation::Init(); //after Attacks
+    Syzygy::Init(Syzygy::DEFAULT_PATH);
     ZobristKeys::Init();
+    nnue.Load();
 
-    UCI_CLASSICAL_EVAL = true;
+    UCI_CLASSICAL_EVAL = false;
 
     std::string mode;
     int concurrency = 0;
 
+    // m: mode: ['games', 'random', or 'benchmark']
+    // c: concurrency
     std::span<char*> args(argv, argc);
     for (size_t i = 1; i < args.size(); ++i) {
         std::string_view arg = args[i];
@@ -33,6 +39,8 @@ int main(int argc, char** argv) {
 
     GenSFen gensfen;
     gensfen.Run(mode, concurrency);
+
+    Syzygy::Free();
 
     return 0;
 }
