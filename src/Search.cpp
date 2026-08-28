9c78a816fc801632683849e72770cba328eac243
Carlos Sanchez Mayordomo <karshan7@gmail.com>
2026-08-28 08:12:27 +0200

    rfp 90

--- src/Search.cpp
+++ src/Search.cpp
@@ -444,7 +444,7 @@ int Search::NegaMax(Board &board, int depth, int alpha, int beta) {
 
     // --- Reverse Futility Pruning ---
     // Prune if static evaluation is too good (eval >> beta)
-    const int staticMargin = 120;
+    const int staticMargin = 90;
     if(depth <= 4 && !isPV && !inCheck) {
         int staticEval = eval - depth * staticMargin;
         if(staticEval >= beta) {
